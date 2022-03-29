fc2d39e97d22e0345b30f9fb28243ca3840e8379
joanderson <joandersongsantos@yahoo.com>
2022-03-29 20:50:18 -0300

    chore: Initial project setup

---
+++ src/main/java/dev/joanderson/Logana/LoganaApplication.java
@@ -0,0 +1,13 @@
+package dev.joanderson.Logana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoganaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoganaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/joanderson/Logana/LoganaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.joanderson.Logana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoganaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
